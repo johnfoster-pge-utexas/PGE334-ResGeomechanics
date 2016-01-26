a6b7de978229083329146f9dfc36455d38cf6168
John Foster <johntfosterjr@gmail.com>
2016-01-26 11:52:52 -0600

    Add Lecture recordings, slides, pdfs from first few lectures

--- lectures/convert.sh
+++ lectures/convert.sh
@@ -2,8 +2,8 @@
 
 #Local
 #ipython nbconvert $1 --to slides --reveal-prefix ~/projects/reveal.js --config slides_config.py --post serve
-#Own hosted
-#ipython nbconvert $1 --to slides --reveal-prefix https://rawgit.com/johntfoster/reveal.js/3.0.0.1 --config slides_config.py --post serve
+#Own hosted (debug)
+#ipython nbconvert $1 --to slides --reveal-prefix https://rawgit.com/johntfoster/reveal.js/tree/master --config slides_config.py --post serve
 #Production CDN
 jupyter nbconvert $1 --to slides --reveal-prefix https://cdn.rawgit.com/johntfoster/reveal.js/3.0.0.1 --config slides_config.py --post serve
 
